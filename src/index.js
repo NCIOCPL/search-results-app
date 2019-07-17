befb5821f3c75c4395a954edc4913f7a2a2f1830
Brendan Beltz <bbeltz@sapient.com>
2019-07-17 13:56:15 -0400

    Add boilerplate for primary view

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import * as reducers from './state/store/reducers';
 import createEventReporterMiddleware from './state/middleware/eventReporter';
+import Results from './views/results';
 import './index.css';
 
 const initialize = ({
@@ -60,7 +61,7 @@ const initialize = ({
   const App = () => {
     return (
       <Provider store={ store }>
-        <div>Search Results App</div>
+        <Results />
       </Provider>
     );
   }
