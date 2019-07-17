09d97288b40fab7141e6a242decfbc7006b840f0
Brendan Beltz <bbeltz@sapient.com>
2019-07-17 13:19:25 -0400

    Skeleton of initial params for app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const initialize = () => {
+const initialize = (
+  // appId = '@@/DEFAULT_APP_ID',
+  // useSessionStorage = true,
+  // rootId = 'r4r-root',
+  // eventHandler,
+  // services = {
+  //   search: {
+  //     ?endpoint [/Search],
+  //     params: {
+  //       collection [cgov|doc],
+  //       site [all|microsite URL],
+  //       language: [en|es]
+  //     }
+  //   },
+  //   dictionary: {
+  //     ?endpoint [/Dictionary.Service/v1/search],
+  //     params: {
+  //       dictionary: [term] equivalent to collection,
+  //       language: [English|Spanish]
+  //       searchType: "exact",
+  //     }
+  //   },
+  //   bestBets: {
+  //     ?endpoint [],
+  //     params: {
+  //       language: [en|es],
+  //       collection [live|preview],
+  //     }
+  //   }
+  // }
+  // language,
+) => {
 
   const App = () => {
     return <div>Search Results App</div>;
