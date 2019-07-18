2f88e36a409e978483cd1c9bd165c7a6834e4a17
Brendan Beltz <bbeltz@sapient.com>
2019-07-18 11:59:32 -0400

    Add skeleton reducers for cache, results, and errors

--- src/state/store/reducers.js
+++ src/state/store/reducers.js
@@ -1,3 +1,6 @@
 // Import all the reducers and reexport them.
 // When the app is instantiated all the exported files here will be combined in a call to combineReducers
 // in addition to any third party reducers tha may need to be passed in.
+export { reducer as cache } from './cache';
+export { reducer as error } from './error';
+export { reducer as results } from './results';
