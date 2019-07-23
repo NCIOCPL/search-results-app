f0ccf70a601272188c4e024922ce40831efe3785
Brendan Beltz <bbeltz@sapient.com>
2019-07-23 08:41:29 -0400

    Add legacy logic for splitting dictionary definition text

--- src/components/feature-box/dictionary/Dictionary.jsx
+++ src/components/feature-box/dictionary/Dictionary.jsx
@@ -11,7 +11,21 @@ const Dictionary = () => {
     definition,
     pronunciation,
   } = dictionary;
+  // The definition text is split at the end of the first sentence and the
+  // remainder hidden in a dropdown. We need to prepare for that. The odd use of '. ' to
+  // identify the end of the first sentence is a bit of logic we're going to reuse from
+  // bestBets.js as it has presumably been well vetted against the existing dataset (as well
+  // as a bit more.)
+  const definitionHtml = definition.html;
+	// this is to pull out newline characters which have been found to interfere with the period + blank space method for identifying end of first sentence, ie in "tumor" definition.
+	const sanitizedDefinitionText = definitionHtml.replace(/(\r\n|\n|\r)/gm," ");
+  const endOfFirstSentenceTest = ". ";
+	const firstSentencePartialText =  sanitizedDefinitionText.slice(0, sanitizedDefinitionText.indexOf(endOfFirstSentenceTest) + 1);
+  const definitionRemainderText = sanitizedDefinitionText.slice(sanitizedDefinitionText.indexOf(endOfFirstSentenceTest) + 1);
+  const hasMultipleSentences = firstSentencePartialText !== '';
+
   // TODO: Remove the tables?
+  // TODO: No pronunciation if one doesn't exist.
   return (
     <div className="feature-box__definition">
       <h2>Definition:</h2>
@@ -31,7 +45,11 @@ const Dictionary = () => {
           {' '}{ pronunciation.key }{' '}
         </dd>
         <dd className="definition">
-          { definition.text }
+          <span>{ hasMultipleSentences ? firstSentencePartialText : sanitizedDefinitionText }</span>
+          {
+            hasMultipleSentences &&
+              <span>{ definitionRemainderText }</span>
+          }
           {/* HOW DOES THIS GET DIVIDED?? (probably the stupid jquery 'library') */}
           {/* <span id="definitionStart">
             An abnormal mass of tissue that results when cells divide more than
