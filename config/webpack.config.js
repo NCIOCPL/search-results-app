7d55b5f67817e25983a2949be6501070eed743fb
Brendan Beltz <bbeltz@sapient.com>
2019-07-25 12:23:05 -0400

    Set up webpack output as library

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -166,6 +166,8 @@ module.exports = function(webpackEnv) {
       // We inferred the "public path" (such as / or /my-project) from homepage.
       // We use "/" in development.
       publicPath: publicPath,
+      library: "nci-search-results-app",
+      libraryTarget: "umd",
       // Point sourcemap entries to original disk location (format as URL on Windows)
       devtoolModuleFilenameTemplate: isEnvProduction
         ? info =>
